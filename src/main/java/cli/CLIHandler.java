ea3c4cbd69118e2163b9d415b4da645a8365530a
Anna Guccione <88945042+pannafritta@users.noreply.github.com>
2024-07-08 17:40:00 +0200

    Added the signatures to Handler, CLIHandler and DisplayFormatter

--- src/main/java/cli/CLIHandler.java
+++ src/main/java/cli/CLIHandler.java
@@ -1,4 +1,40 @@
 package cli;
 
-public class CLIHandler {
+import controller.GameController;
+
+public class CLIHandler extends Handler {
+    CommandParser commandParser;
+    DisplayFormatter displayFormatter;
+
+    public CLIHandler() {
+        super();
+    }
+
+    public CLIHandler(GameController gameController, CommandParser commandParser, DisplayFormatter displayFormatter) {
+        super(gameController);
+        this.commandParser = commandParser;
+        this.displayFormatter = displayFormatter;
+    }
+
+    @Override
+    public void launch() {
+    }
+
+    @Override
+    protected void newGame() {
+    }
+
+    @Override
+    protected void setDifficulty() {
+    }
+
+    @Override
+    protected void gameRules() {
+
+    }
+
+    @Override
+    protected void exit() {
+    }
+
 }
---
+++ src/main/java/cli/DisplayFormatter.java
@@ -0,0 +1,11 @@
+package cli;
+
+import model.Board;
+
+public class DisplayFormatter {
+    private String flagSymbol = "🚩";
+    private String bombSymbol = "💣";
+
+    protected String formatBoard(Board board) {
+    }
+}
