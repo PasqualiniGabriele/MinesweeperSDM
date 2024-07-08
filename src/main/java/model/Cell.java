b562b900a4b097b009c698c840c00b4566f6f1bf
lauragallo <gallolaura22@gmail.com>
2024-07-08 21:51:28 +0200

    Added methods reveal and toggleFlag in class Cell

--- src/main/java/model/Cell.java
+++ src/main/java/model/Cell.java
@@ -1,5 +1,29 @@
 package model;
 
 public abstract class Cell {
+    private CellState state;
+
+
+    public Cell() {
+        this.state = new ClosedState();
+    }
+
+    public void setState(CellState state) {
+        this.state = state;
+    }
+
+    public CellState getState() {
+        return state;
+    }
+
+    public void reveal() {
+        state.reveal(this);
+    }
+
     public abstract boolean hasMine();
+
+
+    public void toggleFlag() {
+        state.toggleFlag(this);
+    }
 }
