c92eed8a9c8e8bacf0ff1838fc85a70da3d2c312
Anna Guccione <88945042+pannafritta@users.noreply.github.com>
2024-07-08 16:22:19 +0200

    Added tests to GameControllerTest

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -11,30 +11,3 @@ enum Status {
     ONGOING
 }
 
-enum Difficulty {
-    EASY(10, 8, 8),
-    MEDIUM(40, 16, 16),
-    HARD(99, 30, 16);
-
-    private final int numOfBombs;
-    private final int width;
-    private final int height;
-
-    Difficulty(int numOfBombs, int width, int height) {
-        this.numOfBombs = numOfBombs;
-        this.width = width;
-        this.height = height;
-    }
-
-    public int getNumOfBombs() {
-        return numOfBombs;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public long getHeight() {
-        return height;
-    }
-}
--- src/test/java/controller/GameControllerTest.java
+++ src/test/java/controller/GameControllerTest.java
@@ -1,7 +1,35 @@
 package controller;
 
+import model.Difficulty;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameControllerTest {
 
+    GameController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new GameController();
+    }
+
+    @Test
+    void testCreateEasyGame() {
+        controller.createGame("EASY");
+        assertEquals(controller.getGame().difficulty(), Difficulty.EASY);
+    }
+
+    @Test
+    void testCreateMediumGame() {
+        controller.createGame("MEDIUM");
+        assertEquals(controller.getGame().difficulty(), Difficulty.MEDIUM);
+    }
+
+    @Test
+    void testCreateHardGame() {
+        controller.createGame("HARD");
+        assertEquals(controller.getGame().difficulty(), Difficulty.HARD);
+    }
 }
