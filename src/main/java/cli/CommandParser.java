f7e342ecadfe57c79c8d4528c5543ef7c34e9a20
Gabriele Pasqualini <gabrielepasqualini@outlook.com>
2024-07-08 18:35:20 +0200

    Added different cases to CommandParserTest.java and improved the class readability. Improved the input check of CommandParser.

--- src/main/java/cli/CommandParser.java
+++ src/main/java/cli/CommandParser.java
@@ -5,19 +5,41 @@ import model.Coordinate;
 import java.util.Set;
 
 public class CommandParser {
+
     private static final Set<String> VALID_ACTIONS = Set.of("F", "C");
 
-    public static Command parseCommand(String userInput) throws IllegalArgumentException{
-
-        String[] inputArray = userInput.split(" ");
-        String action = inputArray[0];
-        int x = Integer.parseInt(inputArray[1]);
-        int y = Integer.parseInt(inputArray[2]);
-
-        if (!VALID_ACTIONS.contains(action)) {
-            throw new IllegalArgumentException();
+    public static Command parseCommand(String userInput) throws IllegalArgumentException {
+        String[] commandArray = userInput.split(" ");
+        if (!isValidCommand(commandArray)) {
+            throw new IllegalArgumentException("Invalid command format");
         }
 
+        String action = commandArray[0];
+        int x = Integer.parseInt(commandArray[1]);
+        int y = Integer.parseInt(commandArray[2]);
         return new Command(action, new Coordinate(x, y));
+    }
+
+    private static boolean isValidCommand(String[] commandArray) {
+        if (commandArray.length != 3) {
+            return false;
+        }
+
+        String action = commandArray[0];
+        if (!VALID_ACTIONS.contains(action)) {
+            return false;
+        }
+
+        try {
+            int x = Integer.parseInt(commandArray[1]);
+            int y = Integer.parseInt(commandArray[2]);
+
+            if (x <= 0 || y <= 0) {
+                throw new IllegalArgumentException("Coordinates must be positive");
+            }
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
--- src/test/java/cli/CommandParserTest.java
+++ src/test/java/cli/CommandParserTest.java
@@ -1,33 +1,35 @@
 package cli;
 
 import model.Coordinate;
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommandParserTest {
+
     @Test
-    public void testFlaggingCommandActionParsing() {
-        String input = "F 1 2";
-        Command expected = new Command("F", new Coordinate(1, 2));
+    public void testParseCommandWithValidInputs() {
+        assertValidCommand("F 10 20", new Command("F", new Coordinate(10, 20)));
+        assertValidCommand("C 1 2", new Command("C", new Coordinate(1, 2)));
+    }
+
+    @Test
+    void testParseCommandWithInvalidInputs() {
+        assertInvalidCommand("A 2 2");
+        assertInvalidCommand("F -3 -4");
+        assertInvalidCommand("C 1");
+        assertInvalidCommand("C 1 2 3");
+        assertInvalidCommand("F ten twenty");
+    }
+
+    private void assertValidCommand(String input, Command expected) {
         Command actual = CommandParser.parseCommand(input);
         assertEquals(expected, actual);
     }
 
-    @Test
-    public void testClickCommandActionParsing() {
-        String input = "C 1 2";
-        Command expected = new Command("C", new Coordinate(1, 2));
-        Command actual = CommandParser.parseCommand(input);
-        assertEquals(expected, actual);
+    private void assertInvalidCommand(String input) {
+        assertThrows(IllegalArgumentException.class, () ->
+                CommandParser.parseCommand(input)
+        );
     }
-
-    @Test
-    public void testInvalidCommandActionParsing() {
-        String input = "invalid_input 1 2";
-        assertThrows(IllegalArgumentException.class, () -> {
-            CommandParser.parseCommand(input);
-        });
-    }
-
 }
