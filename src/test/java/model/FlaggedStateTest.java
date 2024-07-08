c70104bb9ab78ecc691bdfa607ee8da7cd2f0f65
lauragallo <gallolaura22@gmail.com>
2024-07-08 22:04:09 +0200

    Test methods reveal and toggleFlag in FlaggedStateTest

--- src/test/java/model/FlaggedStateTest.java
+++ src/test/java/model/FlaggedStateTest.java
@@ -1,7 +1,29 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class FlaggedStateTest {
+class FlaggedStateTest{
 
+    Cell cell;
+
+    @BeforeEach
+    void setUp() {
+        cell = new BombedCell();
+        cell.setState(new FlaggedState());
+    }
+
+    @Test
+    void testStateAfterReveal(){
+        cell.reveal();
+        assertInstanceOf(FlaggedState.class, cell.getState());
+    }
+
+    @Test
+    void testStateAfterToggleFlag(){
+        cell.toggleFlag();
+        assertInstanceOf(ClosedState.class, cell.getState());
+    }
 }
