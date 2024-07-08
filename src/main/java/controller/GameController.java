4a642733aa5cb01cc22d91d29eaa5e1112cf346a
Fede <federicomarenco02@gmail.com>
2024-07-08 16:11:46 +0200

    Updated GameController with signatures

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,4 +1,49 @@
 package controller;
 
+import cli.Command;
+import model.Board;
+import model.Game;
+
 public class GameController {
+
+    private Game game;
+    private Board board;
+
+    private boolean firstClick;
+
+    public GameController() {
+    }
+
+    public void createGame(String difficulty) {
+    }
+
+    public void endGame() {
+    }
+
+    public void applyCommand(Command command) {
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public boolean isFirstClick() {
+        return firstClick;
+    }
+
+    public void setFirstClick(boolean firstClick) {
+        this.firstClick = firstClick;
+    }
 }
