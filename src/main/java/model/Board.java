80f03d2973327e87ab9e0ebebeaec0963261dc31
Fede <federicomarenco02@gmail.com>
2024-07-09 18:06:02 +0200

    Wrote initializeBoard and the getters/setters methods

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -1,4 +1,33 @@
 package model;
 
 public class Board {
+    private Cell[][] cells;
+
+    public Board(int width, int height) {
+        cells = new Cell[width][height];
+    }
+
+    public void initializeBoard() {
+        for (int i = 0; i < getWidth(); i++) {
+            for (int j = 0; j < getHeight(); j++) {
+                cells[i][j] = new FreeCell(0);
+            }
+        }
+    }
+
+    public Cell getCell(Coordinate coordinate) {
+        return cells[coordinate.x()][coordinate.y()];
+    }
+
+    public void setCell(Cell cell, Coordinate coordinate) {
+        cells[coordinate.x()][coordinate.y()] = cell;
+    }
+
+    public int getWidth() {
+        return cells.length;
+    }
+
+    public int getHeight() {
+        return cells[0].length;
+    }
 }
