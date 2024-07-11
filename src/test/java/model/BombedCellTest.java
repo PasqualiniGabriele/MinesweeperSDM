5dcb9d4a4612997846e5f82075e9336a00809034
lauragallo <gallolaura22@gmail.com>
2024-07-11 20:02:57 +0200

    wrote toString method and tests for class BombedCell

--- src/test/java/model/BombedCellTest.java
+++ src/test/java/model/BombedCellTest.java
@@ -3,6 +3,7 @@ package model;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class BombedCellTest {
     Cell cell;
@@ -11,5 +12,26 @@ class BombedCellTest {
     void testIfCellHasMine(){
         cell = new BombedCell();
         assertTrue(cell.hasMine());
+    }
+    @Test
+    void testFlaggedBombedCell(){
+        CellState mockFlaggedState = mock(FlaggedState.class);
+        Cell flaggedCell = new BombedCell();
+        flaggedCell.setState(mockFlaggedState);
+        assertEquals("🚩", flaggedCell.toString());
+    }
+    @Test
+    void testClosedBombedCell(){
+        CellState mockClosedState = mock(ClosedState.class);
+        Cell closedCell = new BombedCell();
+        closedCell.setState(mockClosedState);
+        assertEquals(" ", closedCell.toString());
+    }
+    @Test
+    void testOpenBombedCell(){
+        CellState mockOpenState = mock(OpenState.class);
+        Cell OpenCell = new BombedCell();
+        OpenCell.setState(mockOpenState);
+        assertEquals("💣", OpenCell.toString());
     }
 }
