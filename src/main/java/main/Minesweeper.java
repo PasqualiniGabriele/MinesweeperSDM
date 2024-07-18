0e6b59738a72fbfd5f0a4681b3c1d3406a9c7f0e
lauragallo <gallolaura22@gmail.com>
2024-07-18 15:12:06 +0200

    Moving the game loop to GameController: fixed call from main method

--- src/main/java/main/Minesweeper.java
+++ src/main/java/main/Minesweeper.java
@@ -1,11 +1,11 @@
 package main;
 import cli.CLIHandler;
-import handler.UserHandler;
+import handler.GameController;
 
 
 public class Minesweeper {
     public static void main(String[] args) {
-        UserHandler handler = new CLIHandler();
-        handler.launch();
+        GameController minesweeper = new GameController(new CLIHandler());
+        minesweeper.launch();
     }
 }
