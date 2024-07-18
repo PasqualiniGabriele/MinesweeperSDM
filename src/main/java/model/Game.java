5bf81738b68d737838b865ee8d0bb51eef04d53e
lauragallo <gallolaura22@gmail.com>
2024-07-18 13:22:27 +0200

    Moving the game loop to GameController: Removed field Difficulty from class Game

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -6,13 +6,11 @@ import java.time.LocalDateTime;
 public class Game {
     private final LocalDateTime startTime;
     private LocalDateTime endTime;
-    private final Difficulty difficulty;
     private GameStatus status;
 
-    public Game(Difficulty difficulty) {
+    public Game() {
         startTime = LocalDateTime.now();
         status = GameStatus.ONGOING;
-        this.difficulty = difficulty;
     }
 
     public Duration calculateGameTime() {
@@ -22,10 +20,6 @@ public class Game {
     public void end(GameStatus gameStatus) {
         endTime = LocalDateTime.now();
         status = gameStatus;
-    }
-
-    public Difficulty getDifficulty() {
-        return difficulty;
     }
 
     public GameStatus getStatus() {
