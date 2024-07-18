5ed496968819f05a6494edebebd7b87315ca7976
lauragallo <gallolaura22@gmail.com>
2024-07-18 13:19:41 +0200

    Moving the game loop to GameController: Class CLIHandler implements new methods of class UserHandler. Deleted method menu and test for class CLIHandler

--- src/main/java/cli/CLIHandler.java
+++ src/main/java/cli/CLIHandler.java
@@ -1,10 +1,12 @@
 package cli;
 
-import handler.UserHandler;
+import handler.UIHandler;
+import model.Board;
+import model.Difficulty;
 
 import java.util.Scanner;
 
-public class CLIHandler extends UserHandler {
+public class CLIHandler extends UIHandler {
     Scanner scanner;
     private static final String QUIT_COMMAND = "Q";
 
@@ -14,102 +16,54 @@ public class CLIHandler extends UserHandler {
     }
 
     @Override
-    public void launch() {
+    public void welcome() {
         DisplayFormatter.displayWelcomeScreen();
-        try {
-            menu();
-        } catch (IllegalArgumentException e) {
-            System.out.println("Not a valid command.\nReturning to main menu...");
-            menu();
-        }
-    }
-
-    private void menu() {
-        DisplayFormatter.displayMenu();
-        String input = scanner.nextLine();
-        switch (input) {
-            case "1":
-                setDifficulty();
-                startGame();
-                break;
-            case "2":
-                gameRules();
-                break;
-            case "3":
-                exit();
-                break;
-            default:
-                throw new IllegalArgumentException();
-        }
-    }
-
-    protected void setDifficulty() {
-        DisplayFormatter.displayDifficultyMenu();
-
-        String input = scanner.nextLine();
-        switch (input) {
-            case "1":
-                getGameController().createGame("EASY");
-                break;
-            case "2":
-                getGameController().createGame("MEDIUM");
-                break;
-            case "3":
-                getGameController().createGame("HARD");
-                break;
-        }
     }
 
     @Override
-    protected void startGame() {
-        while (true) {
-            DisplayFormatter.displayBoard(getGameController().getBoard());
-
-            System.out.println("Enter next command or Q to quit game:");
-            String input = scanner.nextLine();
-            if (isQuitCommand(input)) {
-                exit();
-                return;
-            } else {
-                processCommand(input);
-            }
+    public Command hasNextCommand() {
+        String input = scanner.nextLine();
+        if (isQuitCommand(input)) {
+            return null;
         }
+        return CommandParser.parseCommand(input);
     }
 
-    private static boolean isQuitCommand(String input) {
-        return input.equalsIgnoreCase(QUIT_COMMAND);
+    @Override
+    public Difficulty askForDifficulty() {
+        DisplayFormatter.displayDifficultyMenu();
+        String input = scanner.nextLine();
+        return switch (input) {
+            case "1" -> Difficulty.EASY;
+            case "2" -> Difficulty.MEDIUM;
+            case "3" -> Difficulty.HARD;
+            default -> Difficulty.MEDIUM;
+        };
     }
 
-    private void processCommand(String input) {
-        try {
-            getGameController().applyCommand(CommandParser.parseCommand(input));
-        } catch (IllegalArgumentException e) {
-            System.out.println("""
-                    Not a valid command:
-                    Game Commands:
-                    - To uncover a cell: Type 'C x y'
-                    - To flag/unflag a cell: Type 'F x y'
-                    - where (x, y) are the coordinates of the cell.
-                    """);
-        }
+    @Override
+    public void show(Board board) {
+        DisplayFormatter.displayBoard(board);
     }
 
     @Override
     protected void gameRules() {
         DisplayFormatter.displayGameRules();
-
         scanner.nextLine();
-        menu();
     }
 
     @Override
-    protected void exit() {
+    public void exit() {
         System.out.println("Thank you for playing!");
     }
 
     // for testing:
     public void setScanner(Scanner scanner) {
         this.scanner = scanner;
+    }
+
+    private static boolean isQuitCommand(String input) {
+        return input.equalsIgnoreCase(QUIT_COMMAND);
     }
 
 }
