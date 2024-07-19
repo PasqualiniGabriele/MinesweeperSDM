cefbb8137d6fe67b51fc1c6bc5243b53853b2533
Gabriele Pasqualini <gabrielepasqualini@outlook.com>
2024-07-19 16:12:36 +0200

    Wrong command format handled with a try catch in CLIHandler

--- src/main/java/cli/CLIHandler.java
+++ src/main/java/cli/CLIHandler.java
@@ -26,7 +26,12 @@ public class CLIHandler extends UIHandler {
         if (isQuitCommand(input)) {
             return null;
         }
-        return CommandParser.parseCommand(input);
+        try {
+            return CommandParser.parseCommand(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Not a valid command\nValid format: C x y or F x y");
+            return hasNextCommand();
+        }
     }
 
     @Override
