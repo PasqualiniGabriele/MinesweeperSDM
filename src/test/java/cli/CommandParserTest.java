77e4b5e16f4bd2c1a77a5a4bb17830006d493159
lauragallo <gallolaura22@gmail.com>
2024-07-20 21:51:54 +0200

    test for method parseCommand

--- src/test/java/cli/CommandParserTest.java
+++ src/test/java/cli/CommandParserTest.java
@@ -4,6 +4,7 @@ import handler.Command;
 import handler.GameCommand;
 import model.Coordinate;
 
+import model.Game;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,8 +12,9 @@ class CommandParserTest {
 
     @Test
     public void testParseCommandWithValidInputs() {
-        assertValidCommand("F 10 20", new GameCommand("F", new Coordinate(9, 19)));
-        assertValidCommand("C 1 2", new GameCommand("C", new Coordinate(0, 1)));
+        assertValidGameCommand("F 10 20", new GameCommand("F", new Coordinate(9, 19)));
+        assertValidGameCommand("C 1 2", new GameCommand("C", new Coordinate(0, 1)));
+        assertValidCommand("Q", new Command("Q"));
     }
 
     @Test
@@ -22,9 +24,15 @@ class CommandParserTest {
         assertInvalidCommand("C 1");
         assertInvalidCommand("C 1 2 3");
         assertInvalidCommand("F ten twenty");
+        assertInvalidCommand("F");
     }
 
-    private void assertValidCommand(String input, GameCommand expected) {
+    private void assertValidCommand(String input, Command expected) {
+        Command actual = CommandParser.parseCommand(input);
+        assertEquals(actual.getAction(), expected.getAction());
+    }
+
+    private void assertValidGameCommand(String input, GameCommand expected) {
         GameCommand actual = (GameCommand) CommandParser.parseCommand(input);
         assertEquals(actual.getAction(), expected.getAction());
         assertEquals(actual.getCoordinate(), expected.getCoordinate());
