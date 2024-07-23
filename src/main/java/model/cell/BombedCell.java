8ebbd56eb20370bfd1e23f30c8f6163f84351fdd
lauragallo <gallolaura22@gmail.com>
2024-07-23 19:40:47 +0200

    Added JavaDoc class BombedCell

--- src/main/java/model/cell/BombedCell.java
+++ src/main/java/model/cell/BombedCell.java
@@ -2,23 +2,44 @@ package model.cell;
 
 import static cli.ConsoleColor.*;
 
+/**
+ * The {@code BombedCell} class represents a cell that contains a bomb.
+ * This cell is a specific type of {@code Cell} that displays a bomb icon and has
+ * specific behavior when revealed.
+ */
 public class BombedCell extends Cell {
-    public final String BOMB = RED + "●" + RESET;
 
+    public static final String BOMB_ICON = RED + "●" + RESET;
+
+    /**
+     * Constructs a {@code BombedCell} instance.
+     * Initializes the cell with the bomb icon.
+     */
     public BombedCell() {
         super();
-        super.setIcon(BOMB);
+        setIcon(BOMB_ICON);
     }
 
+    /**
+     * Determines if this cell contains a mine (bomb).
+     *
+     * @return {@code true} as this cell always contains a bomb
+     */
     @Override
     public boolean hasMine() {
         return true;
     }
 
+    /**
+     * Reveals the cell. If the cell is in the closed state, it triggers a bomb reveal event
+     * through the event manager, if available, and then performs the standard reveal operation.
+     * Overrides the {@code reveal} method from the {@code Cell} class to handle the bomb-specific behavior.
+     */
     @Override
     public void reveal() {
-        if (isClosedCell() && eventManager != null)
-            eventManager.onBombReveal();
+        if (isClosedCell() && getEventManager() != null) {
+            getEventManager().onBombReveal();
+        }
         super.reveal();
     }
 }
