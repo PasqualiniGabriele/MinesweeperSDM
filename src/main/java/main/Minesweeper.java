d5c35cb8e052afabb0da8a200c2e0be4b472521b
Anna Guccione <88945042+pannafritta@users.noreply.github.com>
2024-07-23 21:55:13 +0200

    Added documentation in main class

--- src/main/java/main/Minesweeper.java
+++ src/main/java/main/Minesweeper.java
@@ -3,8 +3,16 @@ package main;
 import cli.CLIHandler;
 import handler.game.GameController;
 
-
+/**
+ * The entry point for the Minesweeper game application.
+ */
 public class Minesweeper {
+
+    /**
+     * The main method to launch the Minesweeper game.
+     *
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         GameController minesweeper = new GameController(new CLIHandler());
         minesweeper.launch();
