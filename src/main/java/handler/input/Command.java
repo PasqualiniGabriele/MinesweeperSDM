fe61ff24f0a12a4c1335112db9a4d303e1a7fbf9
Fede <federicomarenco02@gmail.com>
2024-07-23 21:01:11 +0200

    Added JavaDoc comment for Command.

--- src/main/java/handler/input/Command.java
+++ src/main/java/handler/input/Command.java
@@ -1,15 +1,37 @@
 package handler.input;
 
+/**
+ * The {@code Command} class represents a command with a specific action.
+ */
 public class Command {
 
-    public enum Action {FLAG_ACTION, CLICK_ACTION, QUIT_ACTION, INFO_ACTION, EASTER_EGG_ACTION}
+    /**
+     * The {@code Action} enum representing the possible actions of a command.
+     */
+    public enum Action {
+        FLAG_ACTION,
+        CLICK_ACTION,
+        QUIT_ACTION,
+        INFO_ACTION,
+        EASTER_EGG_ACTION
+    }
 
     Action action;
 
+    /**
+     * Constructs a new {@code Command} with the specified action.
+     *
+     * @param action The action associated with this command.
+     */
     public Command(Action action) {
         this.action = action;
     }
 
+    /**
+     * Returns the action associated with this command.
+     *
+     * @return The action associated with this command.
+     */
     public Action getAction() {
         return action;
     }
